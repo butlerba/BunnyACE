8216708ed0536047bb26996503e5f6c11ac87c8c
Brett Butler <butlerba@live.com>
2026-02-05 08:30:01 -0500

    attempting to fix slow sleep duration

--- extras/ace.py
+++ extras/ace.py
@@ -819,8 +819,8 @@ class BunnyAce:
         while not bool(sensor_extruder.runout_helper.filament_present):
             self.log_always('ACE:loop monotonic:' + str(self.reactor.monotonic()))
             self.log_always('ACE:loop diff:' + str(self.reactor.monotonic() - start_fast_feed))
-            self.log_always('ACE:loop threshold:' + str(self.toolchange_feed_length//self.feed_speed))
-            if (start_fast_feed and (self.reactor.monotonic() - start_fast_feed) >= (self.toolchange_feed_length//self.feed_speed)):
+            self.log_always('ACE:loop threshold:' + str(self.toolchange_feed_length*0.75))
+            if (start_fast_feed and (self.reactor.monotonic() - start_fast_feed) >= (self.toolchange_feed_length*0.75)):
                 self._set_feeding_speed(tool, self.toolhead_homing_speed)
                 start_fast_feed = 0
 
